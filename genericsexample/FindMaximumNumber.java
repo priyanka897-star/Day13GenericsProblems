bf86206052aeed510c5eade73e2b36f7f0389953
priyanka897-star <priyankamore8997@gmail.com>
2021-10-14 16:20:54 +0530

    Added:Functionality to add maximum number using generic method and constructor

--- genericsexample/FindMaximumNumber.java
+++ genericsexample/FindMaximumNumber.java
@@ -3,7 +3,18 @@ package genericsexample;
 public class FindMaximumNumber<T extends Comparable<T>> {
 	T x, y, z;
 
-	public static <T extends Comparable<T>> T findMaximumNumber(T x, T y, T z) {
+	public FindMaximumNumber(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T maximum() {
+		return FindMaximumNumber.maximum(x, y, z);
+
+	}
+
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
 		T max = x;
 		if (y.compareTo(max) > 0) {
 			max = y;
@@ -14,9 +25,33 @@ public class FindMaximumNumber<T extends Comparable<T>> {
 		return max;
 	}
 
+	public static String testMaximum(String x, String y, String z) {
+
+		String max = y;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Maximum  Number using Integer type are: " + findMaximumNumber(45, 67, 27));
-		System.out.println("Maximum  Number using Double type are: " + findMaximumNumber(123.32, 89.67, 75.27));
-		System.out.println("Maximum  String  using String type are: " + findMaximumNumber("Apple", "Banana", "Orange"));
+		Integer xInt = 4, yInt = 5, zInt = 6;
+		Double xDouble = 45.23, yDouble = 67.25, zDouble = 12.89;
+		String xString = "Apple", yString = "Banana", zString = "Orange";
+
+		String maximum = FindMaximumNumber.testMaximum(xString, yString, zString);
+		System.out.println("Maximum String are :" + maximum);
+
+		int maxInt = new FindMaximumNumber<Integer>(xInt, yInt, zInt).maximum();
+		System.out.println("Maximum Number are :" + maxInt);
+
+		double maxDouble = new FindMaximumNumber<Double>(xDouble, yDouble, zDouble).maximum();
+		System.out.println("Maximum Number are :" + maxDouble);
+
+		String maxString = new FindMaximumNumber<String>(xString, yString, zString).maximum();
+		System.out.println("Maximum String are :" + maxString);
 	}
 }
